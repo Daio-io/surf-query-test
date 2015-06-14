68124fe198d758634f4db8f79ad8305b509592a4
Daveloper87 <dai.williams@bbc.co.uk>
2015-06-14 14:35:41 +0100

    Added database connection settings

---
+++ api/conf/db/db.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+
+    dev: {
+        connectionString: 'mongodb://localhost/surfify',
+
+        options: {
+            server: {
+                socketOptions: {
+                    keepAlive: 1
+                }
+            }
+        }
+    },
+
+    prod: {
+        connectionString: process.env.DB_CONNECTION,
+
+        options: {
+            server: {
+                socketOptions: {
+                    keepAlive: 1
+                }
+            }
+        }
+    }
+
+};
+
--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 var http = require('http');
 var koa = require('koa');
 var app = koa();
+var settings = require('./api/conf/api.settings');
+
+// Bootstrapping
+require('./api/conf/api.bootstrap')();
+
+// Routes
+require('./api/conf/api.routes')(app);
 
 var server = http.createServer(app.callback());
 
-server.listen(3000, function() {
+server.listen(settings.port, function() {
 
-    console.log('Surf-Query Started on:', 3000);
+    console.log('Surf-Query Started on:', settings.port);
 
 });
