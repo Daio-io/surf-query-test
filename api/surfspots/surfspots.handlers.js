578cf670da910960505959e7bf3d26dd1307b269
Daveloper87 <dai.williams@bbc.co.uk>
2015-06-14 14:49:51 +0100

    updated handlers

--- api/surfspots/surfspots.handlers.js
+++ api/surfspots/surfspots.handlers.js
@@ -5,7 +5,7 @@ var SurfSpotModel = require('./surfspots.model');
 exports.getSurfSpot = function *() {
 
     let search = new RegExp(this.query.q, 'i');
-    let query = SurfSpotModel.find({location: search});
+    let query = SurfSpotModel.find({spot_name: search});
 
     this.body = yield query.exec();
 
